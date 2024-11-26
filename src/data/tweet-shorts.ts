8834202505b83937be3686eceff6e12de08fae90
Al <ali.al81@ui.ac.id>
2024-11-26 09:53:50 +0700

    Set tweet embed list on its own ts file

---
+++ src/data/tweet-shorts.ts
@@ -0,0 +1,157 @@
+type TweetEmbed = string;
+
+export const tweetEmbeds: TweetEmbed[] = [
+
+    `<blockquote class="twitter-tweet"><p lang="en" dir="ltr">&quot;Everything will break.<br><br>Entropy, the steady decline into disorder, that&#39;s a fundamental part of the universe.<br><br>Entropy will get us all in the end.<br><br>But that doesn&#39;t mean you shouldn&#39;t build things anyway.&quot;</p>&mdash; Al (@alhrkn) <a href="https://twitter.com/alhrkn/status/1584363585605496832?ref_src=twsrc%5Etfw">October 24, 2022</a></blockquote>`
+,
+    `<blockquote class="twitter-tweet"><p lang="en" dir="ltr">The dominant form of internet content is shifting: from picture and text, to video —enabled by the proliferation of high-speed internet.<br><br>Some might worry that it would make the future generations become less interested in reading, given how easy it is to just listen &amp; watch.</p>&mdash; Al (@alhrkn) <a href="https://twitter.com/alhrkn/status/1438888187036794881?ref_src=twsrc%5Etfw">September 17, 2021</a></blockquote>`
+,
+    `<blockquote class="twitter-tweet"><p lang="in" dir="ltr">Agaknya ada update ciri baru Dunning-Kruger effect di jaman media sosial sekarang: OVER SHARING setelah baru sebentar belajar.<br><br>Contoh: setelah ikutan kursus digital marketing gratisan sekali, langsung jd kritikus konten di medsos, juga &#39;berbagi&#39;/ngajarin tapi gak ada portofolio. <a href="https://t.co/4bHexB2OBV">pic.twitter.com/4bHexB2OBV</a></p>&mdash; Al (@alhrkn) <a href="https://twitter.com/alhrkn/status/1401828390039015424?ref_src=twsrc%5Etfw">June 7, 2021</a></blockquote>`
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="in" dir="ltr">
+    Setelah 5 tahun di Jakarta, awal tahun ini saya kembali ke Jawa Timur.<br
+    />,Satu hal yg menarik: kampus-kampus perguruan tinggi di sini
+    berlomba membangun gedung-gedung tinggi, menara-menara megah.,<br
+    />Yang sedihnya, hampir semua gedung baru itu kosong di 2 tahun pandemi.
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1526933166597935105?ref_src=twsrc%5Etfw"
+    >May 18, 2022</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    A thread of legendary mathematician stories:,,#1 - Jon von
+    Neumann <a href="https://t.co/vVga9OLigt">https://t.co/vVga9OLigt</a>
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1430491571519057927?ref_src=twsrc%5Etfw"
+    >August 25, 2021</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    The computer now is so good at recognizing our faces with half of it
+    covered, thanks to the current pandemic.,,Services like Google
+    easily matched our bare face photos prior to 2020 with our faces
+    throughout the pandemic, masked.,,Billions of datasets. Leap
+    of accuracy. <a href="https://t.co/tGDzVgtYTI"
+      >pic.twitter.com/tGDzVgtYTI</a
+    >
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1494865215191588868?ref_src=twsrc%5Etfw"
+    >February 19, 2022</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    How do we leverage AI —and its derivatives, to improve science
+    communication?
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1461501420008394754?ref_src=twsrc%5Etfw"
+    >November 19, 2021</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    I used to create videos everyday for 2+ years straight.,,My
+    end goal was to learn filmmaking, but I knew nothing. So I started with
+    what I had: my crappy phone.,,Read no books, watched no
+    tutorials. Just kept shooting &amp; meticulously editing whatever.<br
+    />,It was the most meaningful.. <a href="https://t.co/7sjQG2DyDu"
+      >https://t.co/7sjQG2DyDu</a
+    >
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1450779651580698629?ref_src=twsrc%5Etfw"
+    >October 20, 2021</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    <a href="https://twitter.com/Apple?ref_src=twsrc%5Etfw">@Apple</a> doesn&#39;t
+    hard sell their products on IG.,,Instead, we enjoy the result
+    of their products quality —great photos &amp; videos.,,And
+    this marketing approach leads to their undefeated sales number.,<br
+    />Another reminder that people don&#39;t like to be told to buy
+    something.,,Let &#39;em feel. <a
+      href="https://t.co/3tp6K6JQza">pic.twitter.com/3tp6K6JQza</a
+    >
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1435078828259373063?ref_src=twsrc%5Etfw"
+    >September 7, 2021</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    A Life-long Thread of Gratitude,,I&#39;m grateful for having
+    family who never stops checking on me, remember the tiniest detail of
+    stories that I told them, reminding me to breathe whenever I&#39;m too
+    hard on myself, and being supportive in their own ways.,,1/
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1418209085791510535?ref_src=twsrc%5Etfw"
+    >July 22, 2021</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    Most of our parents gen didn&#39;t know the potential of the internet
+    back in early 90s.,,The image of world-wide interconnected
+    computers via hi-speed connection was hard to comprehend back when
+    average PC was so slow.,,Now, the world &amp; economy are
+    inseparable from the internet.
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1356493528356900864?ref_src=twsrc%5Etfw"
+    >February 2, 2021</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    All the ideas that ever came to my mind but never to execute *easily a
+    thread.,,1: Git for Academic Writing (Paper, Thesis,
+    Dissertation, or any writing projects),,It&#39;s a bit crazy
+    how it&#39;s not yet widely well-known in the academic community, esp.
+    the social studies one.
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1327205937271115777?ref_src=twsrc%5Etfw"
+    >November 13, 2020</a
+  >
+</blockquote>`
+,    `<blockquote class="twitter-tweet">
+  <p lang="en" dir="ltr">
+    Okay, since I like to google things that seems trivial out of daily
+    experiences *or just random thoughts, I&#39;m starting a thread of
+    those. <a href="https://t.co/BsgYCvkGaN">https://t.co/BsgYCvkGaN</a>
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1308629643264626688?ref_src=twsrc%5Etfw"
+    >September 23, 2020</a
+  >
+</blockquote>`
+
+,
+    `<blockquote class="twitter-tweet">
+  <p lang="in" dir="ltr">
+    Penasaran, gimana sih caranya suatu lembaga survei tuh bikin hasil
+    survei biar &#39;sesuai pesanan&#39;? e.g. calon X pingin keliatan
+    leading elektabilitas.,,Mereka beneran survei, lalu
+    analisisnya dikarang? Atau mereka memang sengaja picky di samplingnya
+    sejak awal?,,Atau gimana?
+  </p>&mdash; Al (@alhrkn) <a
+    href="https://twitter.com/alhrkn/status/1714642300326797633?ref_src=twsrc%5Etfw"
+    >October 18, 2023</a>
+</blockquote>`
+]
